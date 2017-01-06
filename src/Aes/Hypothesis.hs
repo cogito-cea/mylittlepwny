63c6c53f362ada816a30578c9a68e1124c330476
Damien Couroussé <damien.courousse@cea.fr>
2017-01-07 00:16:01 +0100

    Aes.Hypothesis: firstSBOX. Compute the output of the first SBOX

--- src/Aes/Hypothesis.hs
+++ src/Aes/Hypothesis.hs
@@ -30,6 +30,10 @@ initState byte ts =
   let keys = keyHypothesis byte
   in  [fmap (`aesInit` t) keys | t <- ts]
 
+-- | Compute the output of the first SBOX
+firstSBOX :: Key -> Plaintext -> State
+firstSBOX k t = (aesInit k t) $$ addRoundKey $$ subBytes
+
 -- | Considering the key byte 'byte' attacked (see 'keyHypothesis'),
 --   compute the matrix of hypothetical values at the output of the
 --   first SBOX, according to the input list of plaintexts 'ts'.
