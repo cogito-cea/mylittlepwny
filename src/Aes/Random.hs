3e1ec96536b2eb705d3b596ddad8f3f7cecc3eae
Damien Couroussé <damien.courousse@cea.fr>
2017-01-07 00:23:54 +0100

    Aes.Random: export a few random helper functions

---
+++ src/Aes/Random.hs
@@ -0,0 +1,20 @@
+module Aes.Random
+  ( randomPlaintexts
+  , randomPlaintexts'
+  ) where
+
+import           System.Random
+
+import           Aes.Types
+
+-- | Compute a lazy list of random 'Plaintext' values.
+--   Because of lazyness, calling this function several times will
+--   produce the same list of values.  To get different lists at each
+--   call, use 'randomPlaintexts'' instead.
+randomPlaintexts :: IO [Plaintext]
+randomPlaintexts = randoms <$> getStdGen
+
+-- | Compute a list of 'n' random 'Plaintext' values.
+--   Each call to this function produces a list of different values.
+randomPlaintexts' :: Int -> IO [Plaintext]
+randomPlaintexts' n = sequence $ take n $ repeat randomIO
