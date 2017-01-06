7870c8439502363f08858ff1071626cbc9958d51
Damien Couroussé <damien.courousse@cea.fr>
2017-01-06 21:28:09 +0100

    Aes.Hypothesis. document the module

--- src/Aes/Hypothesis.hs
+++ src/Aes/Hypothesis.hs
@@ -8,8 +8,11 @@ import           Aes.Types
 import           AesImport
 import           AesReference
 
--- | Generate a set of key hypothesis for attacking AES-128 with
---  CPA/DPA on byte 'Byte'.
+-- | Generate a full set of (256) AES key hypothesis on byte number 'Byte'.
+--   All the other byte values are set to zero.
+--
+--   'byte' is a 'Byte', i.e. an 'Int' value expected between 0 and
+--   15, 23, 31 included for AES-128, AES-196 and AES-256 respectively.
 keyHypothesis :: Byte -> [Key]
 keyHypothesis byte =
   let pre = replicate byte 0
@@ -18,21 +21,29 @@ keyHypothesis byte =
       keyw32 = map tow32 keybytes
   in  map (Key128 . RawKey) keyw32
 
+-- | Considering the key byte 'byte' attacked (see 'keyHypothesis'),
+--   generate the matrix of hypothetical AES init states according to
+--   the input list of plaintexts 'ts'.
 initState :: Byte -> [Plaintext] -> [[State]]
 initState byte ts =
   let keys = keyHypothesis byte
   in  [fmap (`aesInit` t) keys | t <- ts]
 
+-- | Considering the key byte 'byte' attacked (see 'keyHypothesis'),
+--   compute the matrix of hypothetical values at the output of the
+--   first SBOX, according to the input list of plaintexts 'ts'.
 firstRoundSBOX :: Byte -> [Plaintext] -> [[Word8]]
 firstRoundSBOX byte ts =
   let states = initState byte ts
       firstRound = (fmap . fmap) (subBytes . addRoundKey) states
   in  (fmap.fmap) (getByte byte . toAesText) firstRound
 
+-- | Power model: compute the hamming weight.
 hammingWeight :: [[Word8]] -> [[Int]]
 hammingWeight = (fmap.fmap) popCount
 
-
+-- | Export the matrix of CPA/DPA hypothesis in a text file, using a
+--   textual decimal representation.
 exportHypothesis :: FilePath -> [[Int]] -> IO ()
 exportHypothesis f hyp =
   writeFile f $ unlines $ map unwords $ (fmap.fmap) show hyp
