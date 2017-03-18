2ab2b1ba8ba3c83cc484563c376973f9f7ae11c0
Damien Couroussé <damien.courousse@cea.fr>
2017-03-18 01:01:01 +0100

    Aes.Bits: fix function bit for instances Plaintext and State of HasBit

--- src/Aes/Bits.hs
+++ src/Aes/Bits.hs
@@ -38,11 +38,12 @@ instance HasBit Plaintext where
 instance HasBit State where
   bit = bitSt
 
--- Extract the 'n'th bit of the input data. 'Plaintext' stores
--- 'Word8' values with the most significant first.
+-- | Extract the 'n'th bit of the input data. 'Plaintext' stores
+-- 'Word8' values with the _least_ significant first, i.e.
+-- [p0, p1, p2...].
 bitPt :: BitNumber -> Plaintext -> Bit
 bitPt (BitNumber n) (Plaintext ws)
-  | bytePos >= 0 = fromEnum (ws !! bytePos) `shiftR` r .&. 0x1
+  | bytePos >= 0 = fromEnum (reverse ws !! bytePos) `shiftR` r .&. 0x1
   | otherwise    = 0
   where l      = length ws
         -- a Plaintext is a list of Word8 values, i.e. a list of 8-bit
@@ -51,13 +52,13 @@ bitPt (BitNumber n) (Plaintext ws)
         -- is the byte element available in the list or not ?
         bytePos = l-q-1
 
--- Extract the 'n'th bit of the input data. 'State' stores
--- 'Word32' values with the most significant first.
+-- | Extract the 'n'th bit of the input data. 'State' stores
+-- 'Word32' values... in an ankward way.  Actually using a Big Endian thingee.
 bitSt :: BitNumber -> State -> Bit
 bitSt (BitNumber n) (State w0 w1 w2 w3 _)
   | bytePos >= 0 = fromEnum (ws !! bytePos) `shiftR` r .&. 0x1
   | otherwise    = 0
-  where ws = [w0, w1, w2, w3]
+  where ws = [w3, w2, w1, w0]
         l = length ws
         -- a Plaintext is a list of Word32 values, i.e. a list of 32-bit
         -- long values.
