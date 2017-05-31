0658b7e464b5dd73f9b2c456794fc626fa7e3c5d
Damien Couroussé <damien.courousse@cea.fr>
2017-05-31 17:35:27 +0200

    doctest: add tests for AesImport.stringImport

--- src/AesImport.hs
+++ src/AesImport.hs
@@ -44,6 +44,19 @@ class HasAesText a where
   toAesText :: a -> AesText
   fromAesText :: AesText -> a
   -- | Import from Strings. A decimal integer representation is expected.
+  -- >>> stringImport  "00"  :: String
+  -- "0"
+  -- >>> stringImport  ""  :: String
+  -- ""
+  -- >>> stringImport  "0"  :: Key
+  -- *** Exception: unknown key length: 1
+  -- >>> stringImport  "00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15"  :: Key
+  -- Key128 (RawKey [66051,67438087,134810123,202182159])
+  -- >>> stringImport  "00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23"  :: Key
+  -- Key192 (RawKey [66051,67438087,134810123,202182159,269554195,336926231])
+  -- >>> stringImport  "00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31"  :: Key
+  -- Key256 (RawKey [66051,67438087,134810123,202182159,269554195,336926231,404298267,471670303])
+  --
   stringImport :: String -> a
   stringImport = fromAesText . toAesText
   -- | Export to Strings, using a decimal integer textual representation.
