e59e07b9ac3b22379343e001ecef386ff63c8d85
Damien Couroussé <damien.courousse@cea.fr>
2018-11-23 18:38:45 +0100

    TTest.NonSpecific: use conduit

--- src/TTest/NonSpecific.hs
+++ src/TTest/NonSpecific.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE DeriveFunctor              #-}
-{-# LANGUAGE DuplicateRecordFields      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE RecordWildCards            #-}
 
@@ -12,8 +9,9 @@ module TTest.NonSpecific
   , cmdTTestNSParser
   ) where
 
-import           Control.Monad                          (replicateM)
-import           Data.Fold
+import           Conduit
+import qualified Data.ByteString.Char8                  as BC
+import           Data.Maybe                             (catMaybes)
 import           Data.Maybe                             (fromMaybe)
 import qualified Data.Text                              as T
 import qualified Data.Vector.Unboxed                    as U
@@ -25,11 +23,12 @@ import           System.FilePath.Posix                  (takeDirectory, (</>))
 import           Text.Printf                            (printf)
 
 import           CLI.Internal
-import qualified Folds                                  as F
 import qualified Traces                                 as Traces
 
 default (T.Text)
 
+data Trace = Trace Int (Traces.Trace Float)
+  deriving (Eq, Show)
 
 -- | t-test analysis
 ttestNonSpecific :: TTestNonSpecificOptions -> IO ()
@@ -56,41 +55,90 @@ ttestNonSpecific TTestNonSpecificOptions{..} = do
 
   -- TTEST analysis
   --
-  -- TODO we should take nbTraces for each population,
-  --  i.e.,
-  --    - reading lazily the traces
-  --    - sorting the two populations, lazily
-  --    - take nbTraces in each population
-  --  In this case, set the default value of nbTraces to 10000 (in CLI).
-  ts <- replicateM nbTraces $ loadfun tmin tmax
+  -- MAYBE set the default value of nbTraces to 10000 (in CLI).
 
-  -- Each trace is associated to a class, described by an Int number.
-  -- The classesFile describes the class descriptor associated to each
-  -- trace.
-  --
-  -- classes: a list of Int values in {0, 1}.
-  classes <- map read . lines <$> readFile classesFile
+  let loadTraces :: MonadIO m => ConduitT i (Traces.Trace Float) m ()
+      loadTraces = repeatMC (liftIO $ loadfun tmin tmax)
 
-  let partition :: L' (Int, Traces.Trace Float) ([Traces.Trace Float], [Traces.Trace Float])
-      partition = L' done step begin
-      begin = ([], [])
-      step :: ([a], [a]) -> (Int, a) -> ([a], [a])
-      step (p0, p1) (c, x) =
-        if (c == 0)
-        then (p0 ++ [x], p1)
-        else (p0, p1 ++ [x])
-      done = id
+      loadClasses :: MonadResource m => ConduitT i Int m ()
+      loadClasses = sourceFile classesFile
+                     .| concatMapC (catMaybes . map (fmap fst . BC.readInt) . BC.words)
 
-  let (pop0, pop1) = flip run partition $ zip classes ts
+      loadTTestTraces :: MonadResource m => ConduitT () Trace m ()
+      loadTTestTraces = getZipSource
+        $ Trace
+        <$> ZipSource loadClasses
+        <*> ZipSource loadTraces
 
-  let tvals = flip run F.ttestU $ zip pop0 pop1
+      -- Trace selectors
+      selectPop0, selectPop1 :: Trace -> Bool
+      selectPop0 (Trace n _) = n == 0
+      selectPop1 (Trace n _) = n == 1
+
+  tvalues <- runResourceT
+             $ runConduit
+             $ loadTTestTraces
+             .| takeC nbTraces
+             .| ttest selectPop0 selectPop1
 
   -- plot t-test results
   -- ----------------
   let plotOpts = PlotTTest traceDir nbTraces tmin tmax
-  plotTTest plotOpts tvals
+  plotTTest plotOpts tvalues
   return ()
 
+-- | Compute the t-test vector from a stream of 'Trace's
+ttest :: Monad m
+  => (Trace -> Bool) -- ^ trace selector for building the first population of traces
+  -> (Trace -> Bool) -- ^ trace selector for building the second population of traces
+  -> ConduitT Trace Void m (U.Vector Float)
+ttest pop0 pop1 = getZipSink (go <$> population0 <*> population1)
+  where
+    go (Stats n0 m0 s0) (Stats n1 m1 s1) =
+      -- TODO check that n0 == n1
+      U.zipWith (/) (U.zipWith (-) m0 m1)
+                    (U.map sqrt (U.map (/ (fromIntegral $ n0 * n1)) (U.zipWith (+) s0 s1)))
+
+    -- build the partial statistics for each population of traces
+    population0, population1 :: Monad m => ZipSink Trace m Stats
+    population0 = ZipSink $ statsBuilder pop0
+    population1 = ZipSink $ statsBuilder pop1
+
+    -- Filter traces with the predicate function, and compute the
+    -- partial ttest statistics.
+    statsBuilder :: Monad m => (Trace -> Bool) -> ConduitT Trace o m Stats
+    statsBuilder p = filterC p .| foldlC foldStats emptyStats
+
+-- | Accumulator of our statistics that will be used for each
+--   population of traces.
+data Stats = Stats
+             !Int              -- n
+             !(U.Vector Float) -- m
+             !(U.Vector Float) -- s
+emptyStats :: Stats
+emptyStats = Stats 0 U.empty U.empty
+{-# INLINABLE emptyStats #-}
+
+-- | Compute the t-test statistics iteratively: this function computes
+--   the partial average and the partial standard deviation for one
+--   population of traces.
+--
+-- The interface of this function is designed to be used with
+-- 'foldlC'.
+foldStats :: Stats           -- ^ initial state
+          -> Trace           -- ^ the new trace
+          -> Stats           -- ^ the new state
+foldStats (Stats 0 _ _) (Trace _ x) = Stats 1 x zeros
+  where
+    zeros = U.map (const 0) x -- Assuming the two vectors have the same size
+foldStats (Stats n m s) (Trace _ x) = Stats (n+1) m' s'
+  where
+    m' = U.zipWith (+) m $ U.map (/ (fromIntegral n)) $ U.zipWith (-) x m
+    s' = U.zipWith (+) s $ U.zipWith (*) (U.zipWith (-) x m) (U.zipWith (-) x m')
+{-# INLINABLE foldStats #-}
+
+
+-- * plot the t-test results
 
 data PlotTTest = PlotTTest
   { plotDir  :: FilePath
@@ -99,7 +147,6 @@ data PlotTTest = PlotTTest
   , plotTmax :: Int
   }
 
--- | TODO
 plotTTest :: (Num a, PlotValue a, RealFloat a, Show a, U.Unbox a)
   => PlotTTest -> (U.Vector a) -> IO ()
 plotTTest PlotTTest{..} ts = do
