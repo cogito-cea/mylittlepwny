156a2daace8f71b389312ade45116f68fb5fbcc5
Damien Couroussé <damien.courousse@cea.fr>
2018-12-29 21:12:12 +0100

    TTest.Common: expose function closeStats

--- lib/TTest/Common.hs
+++ lib/TTest/Common.hs
@@ -24,21 +24,17 @@ import qualified Data.Vector.Unboxed as U
 import qualified Traces              as Traces
 
 
-data TTTrace = TTTrace !Class !(Traces.Trace Float)
-  deriving (Eq, Show)
+data TTTrace = TTTrace { ttClass :: !Class
+                       , ttTrace :: !(Traces.Trace Float)
+                       } deriving (Eq, Show)
 
 data Class = Pop0 | Pop1
   deriving (Eq, Show)
 
 -- | Compute the t-test vector from a stream of 'Trace's
-ttest :: Monad m => ConduitT TTTrace Void m (U.Vector Float)
-ttest = getZipSink (go <$> population0 <*> population1)
+ttest :: Monad m => ConduitT TTTrace Void m (Traces.Trace Float)
+ttest = getZipSink (closeStats <$> population0 <*> population1)
   where
-    go (Stats n0 m0 s0) (Stats n1 m1 s1) =
-      -- TODO check that n0 == n1
-      U.zipWith (/) (U.zipWith (-) m0 m1)
-                    (U.map sqrt (U.map (/ (fromIntegral $ n0 * n1)) (U.zipWith (+) s0 s1)))
-
     -- build the partial statistics for each population of traces
     population0, population1 :: Monad m => ZipSink TTTrace m Stats
     population0 = ZipSink $ statsBuilder pop0
@@ -47,7 +43,9 @@ ttest = getZipSink (go <$> population0 <*> population1)
     -- Filter traces with the predicate function, and compute the
     -- partial ttest statistics.
     statsBuilder :: Monad m => (TTTrace -> Bool) -> ConduitT TTTrace o m Stats
-    statsBuilder p = filterC p .| foldlC foldStats emptyStats
+    statsBuilder p = filterC p
+                     .| mapC ttTrace
+                     .| foldlC foldStats emptyStats
 
     -- TTTrace selectors
     pop0, pop1 :: TTTrace -> Bool
@@ -73,14 +71,21 @@ emptyStats = Stats 0 U.empty U.empty
 --
 -- The interface of this function is designed to be used with
 -- 'foldlC'.
-foldStats :: Stats           -- ^ initial state
-          -> TTTrace         -- ^ the new trace
-          -> Stats           -- ^ the new state
-foldStats (Stats 0 _ _) (TTTrace _ x) = Stats 1 x zeros
+foldStats :: Stats              -- ^ initial state
+          -> Traces.Trace Float -- ^ the new trace
+          -> Stats              -- ^ the new state
+foldStats (Stats 0 _ _) x = Stats 1 x zeros
   where
     zeros = U.map (const 0) x -- Assuming the two vectors have the same size
-foldStats (Stats n m s) (TTTrace _ x) = Stats (n+1) m' s'
+foldStats (Stats n m s) x = Stats (n+1) m' s'
   where
     m' = U.zipWith (+) m $ U.map (/ (fromIntegral n)) $ U.zipWith (-) x m
     s' = U.zipWith (+) s $ U.zipWith (*) (U.zipWith (-) x m) (U.zipWith (-) x m')
 {-# INLINABLE foldStats #-}
+
+closeStats :: Stats -> Stats -> Traces.Trace Float
+closeStats (Stats n0 m0 s0) (Stats n1 m1 s1) =
+  -- MAYBE check that n0 == n1
+  U.zipWith (/) (U.zipWith (-) m0 m1)
+                (U.map sqrt (U.map (/ (fromIntegral $ n0 * n1)) (U.zipWith (+) s0 s1)))
+{-# INLINABLE closeStats #-}
